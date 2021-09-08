1676f9de157153dd4b1212fccd6aec53c011f386
Sertaç Ayhan <srtc_ayhan@icloud.com>
2021-09-08 13:30:43 +0300

    manifest gradle etc.

---
+++ app/src/main/java/com/example/nasaassignment/NasaApplication.kt
@@ -0,0 +1,8 @@
+package com.example.nasaassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NasaApplication : Application() {
+}
---
+++ app/src/main/java/com/example/nasaassignment/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nasaassignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nasaassignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
